df5dbfe0be86a01c609b36b29671df6b5cfbb238
OneDongua <96672169+OneDongua@users.noreply.github.com>
2024-07-15 00:04:05 +0800

    fix(blog): try fixing bug

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
