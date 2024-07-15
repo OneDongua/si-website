f795c115953d2f9782c9adb27d7292af785cb568
OneDongua <96672169+OneDongua@users.noreply.github.com>
2024-07-15 12:58:31 +0800

    feat(color mode config): add themeConfig/colorMode in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -100,6 +100,11 @@ const config: Config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
   } satisfies Preset.ThemeConfig,
 };
 
