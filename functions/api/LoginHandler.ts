2fdfa993bc3449157435b9879c6acd857a208083
OneDongua <875658697@qq.com>
2024-07-18 13:04:13 +0800

    test login

--- functions/api/LoginHandler.ts
+++ functions/api/LoginHandler.ts
@@ -1,5 +1,5 @@
 interface Env {
-  PART_LIST: KVNamespace;
+  USERS: KVNamespace;
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
@@ -10,7 +10,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
       return new Response("Error: no request body.", { status: 400 });
     }
 
-    return new Response(await context.env.PART_LIST.get(body.email));
+    return new Response(await context.env.USERS.get(body.email));
   }
   return new Response("Error: unknown error", { status: 400 });
 };
