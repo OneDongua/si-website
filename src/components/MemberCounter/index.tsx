e71fb0e33d19c3bb5edb5cfcf840fe7ccf900fbb
OneDongua <875658697@qq.com>
2024-10-26 21:17:35 +0800

    update MemberCounter text

--- src/components/MemberCounter/index.tsx
+++ src/components/MemberCounter/index.tsx
@@ -5,11 +5,14 @@ import styles from "./index.module.css";
 function MemberCounter() {
   return (
     <div className={clsx("card shadow--md", styles.card)}>
-      <caption className={styles.caption}>社团人数</caption>
+      <p>
+        <span className={styles.caption}>社团人数</span>
+        <span className={styles.subCaption}>(不完全统计)</span>
+      </p>
       <table className={styles.table}>
         <thead className={styles.tableHead}>
           <tr className={styles.tableRow}>
-            <th scope="col">社员</th>
+            <th scope="col">新社员</th>
             <th scope="col">管理层</th>
             <th scope="col">总计</th>
           </tr>
