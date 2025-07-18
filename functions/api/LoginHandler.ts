67833b5a471a216f5596e30aabec99711232e186
OneDongua <875658697@qq.com>
2025-07-18 12:58:40 +0800

    test

--- functions/api/LoginHandler.ts
+++ functions/api/LoginHandler.ts
@@ -10,7 +10,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
       return new Response("Error: no request body.", { status: 400 });
     }
 
-    return new Response(await context.env.USERS.get(body.email));
+    return new Response(JSON.stringify(await context.env.USERS.get(body.email)));
   }
   return new Response("Error: unknown error", { status: 400 });
 };
